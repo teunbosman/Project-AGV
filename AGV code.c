e9dbf34da6f3ecd0cad86417fce8878eac68d0b4
TychoWeenink <124277787+TychoWeenink@users.noreply.github.com>
2024-06-04 15:26:25 +0200

    Update AGV code.c
    define gemaakt en extra knop code

--- AGV code.c
+++ AGV code.c
@@ -1,24 +1,44 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+
+#define KNOP1 PF2
+#define KNOP2 PF1
+#define LED1 PB7
+#define LED2 PB6
+
+void init()
+{
+    //led
+    DDRB |= (1 << LED1); //output led 1
+    DDRB |= (1 << LED2); //output led 2
+    PORTB &= ~(1 << LED1); //led 1 uit
+    PORTB &= ~(1 << LED2); //led 2 uit
+	
+    //knoppen
+    PORTF &= ~(1 << KNOP1); //knop 1
+    PORTF &= ~(1 << KNOP2); //knop 2
+    
+	
+}
+
 int main(void)
 {
-    DDRB |= (1<<PB7); //output led 1
-    PORTF &=~(1<<PF2);
-    PORTB &= ~(1<<PB7); //led 1 uit
-	
 	while (1)
 	{
-		if ((PINF &(1<<PF2))==0)
+		if ((PINF & (1 << KNOP1)) !=0)
 		{
-		PORTB |= (1<<PB7); //led 1 aan
+		PORTB |= (1 << LED1); //led aan
 		}
 			
 		else {
-		PORTB &=~(1 << PB7); //led 1 uit
+		PORTB &=~(1 << LED1); //led uit
+		}
+
+		if((PINF & (1 << KNOP1) && (PINF & (1 << KNOP2)) != 0)
+		{
+			PORTB ^= (1 << LED2); //led toggle
 		}
 	}
-
   return 0;
-
 }
