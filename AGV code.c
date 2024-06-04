8809177a8cbe114ccf1c6465f28f5c526f63bd80
JAH1710 <j.a.horsthuis@live.nl>
2024-06-04 14:53:43 +0200

    Update AGV code.c

--- AGV code.c
+++ AGV code.c
@@ -6,14 +6,18 @@ int main(void)
     DDRB |= (1<<PB7); //output led 1
     PORTF &=~(1<<PF2);
     PORTB &= ~(1<<PB7); //led 1 uit
+	
 	while (1)
 	{
 		if ((PINF &(1<<PF2))==0)
 		{
 		PORTB |= (1<<PB7); //led 1 aan
 		_delay_ms(200);
-		PORTB &=~(1 << PB7);
-		_delay_ms(200);
+		}
+			
+		else {
+		PORTB &=~(1 << PB7); //led 1 uit
+		_delay_ms(50);
 		}
 	}
 
